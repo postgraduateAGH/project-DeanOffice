c213781a7cb409d62af582ce317ff5f9bcd53b4b
Marcin Micek <marcin.micek@gmail.com>
2018-03-23 13:32:53 +0100

    First version of Student class with JUnit tests

--- src/main/java/com/mwo/group2/Student.java
+++ src/main/java/com/mwo/group2/Student.java
@@ -17,13 +17,63 @@ public class Student extends User {
 		this.status = status;
 		this.oceny = oceny;
 	}
+	
+	public short getYear() {
+		return year;
+	}
 
+
+	public void setYear(short year) {
+		this.year = year;
+	}
+
+
+	public String getGroup() {
+		return group;
+	}
+
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+
+	public String getLabGroup() {
+		return labGroup;
+	}
+
+
+	public void setLabGroup(String labGroup) {
+		this.labGroup = labGroup;
+	}
+
+
+	public Status getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+
+	public List<Grades> getOceny() {
+		return oceny;
+	}
+
+
+	public void setOceny(List<Grades> oceny) {
+		this.oceny = oceny;
+	}
 	
 	public HashMap<Przedmiot,Oceny> displayResults() {
+		System.out.println("This is displayResults method");
 		return null;
 	}
 	
 	public HashMap<Source,HashMap<Calendar,Boolean>> displayPresences() {
+		System.out.println("This is displayPresences method");
 		return null;
 	}
 }
--- src/test/java/com/mwo/group2/StudentTest.java
+++ src/test/java/com/mwo/group2/StudentTest.java
@@ -1,5 +1,87 @@
 package com.mwo.group2;
 
-public class StudentTest extends User {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 
+public class StudentTest extends User {
+	
+	/*
+    @Before
+    public void initStudents() {
+		Student student = new Student();
+    }
+    */		
+	
+	@Test
+    public void testGetYear() {
+		Student student = new Student();
+		student.setYear((short) 5);
+		Assert.assertEquals(5, student.getYear());
+    }
+ 
+	@Test
+    public void testSetYear() {
+		Student student = new Student();
+		student.setYear((short) 5);
+		Assert.assertEquals(5, student.getYear());
+    }
+	
+    @Test
+    public void testGetGroup() {
+    	Student student = new Student();
+    	student.setGroup("test");
+    	Assert.assertEquals("test", student.getGroup());
+    }
+ 
+    @Test
+    public void testSetGroup() {
+    	Student student = new Student();
+    	student.setGroup("test");
+    	Assert.assertEquals("test", student.getGroup());
+    }
+        
+    @Test
+    public void testGetLabGroup() {
+    	Student student = new Student();
+    	student.setLabGroup("test");
+    	Assert.assertEquals("test", student.getLabGroup());
+    }
+     
+    @Test
+    public void testSetLabGroup() {
+    	Student student = new Student();
+    	student.setLabGroup("test");
+    	Assert.assertEquals("test", student.getLabGroup());
+    }
+    
+    /*
+    @Test
+    public void testGetStatus() {
+    	Student student = new Student();
+    	student.setStatus("test");
+    	Assert.assertEquals("test", student.getStatus());
+    }
+     
+    @Test
+    public void testSetStatus() {
+    	Student student = new Student();
+    	student.setStatus("test");
+        Assert.assertEquals("test", student.getStatus());
+    }
+    
+    @Test
+    public void testGetOceny() {
+    	Student student = new Student();
+    	student.setOceny("test");
+    	Assert.assertEquals("test", student.getOceny());
+    }
+    
+    @Test
+    public void testSetOceny() {
+    	Student student = new Student();
+    	student.setOceny("test");
+    	Assert.assertEquals("test", student.getOceny());
+    }
+	*/
 }
