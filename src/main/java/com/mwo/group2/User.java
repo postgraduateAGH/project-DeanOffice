079c2aedc8ac2ea81d9d42010f4da63a9d77cc98
krenton <pawelkrentowski@gmail.com>
2018-03-27 21:34:21 +0200

    set fields to private and changed order

--- src/main/java/com/mwo/group2/User.java
+++ src/main/java/com/mwo/group2/User.java
@@ -3,35 +3,10 @@ package com.mwo.group2;
 import java.util.List;
 
 public abstract class User {
-    int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    
-    String name;
-    String surname;
-    List<Message> messages;
+    int id;  
+    private String name;
+    private String surname;
+    private List<Message> messages;  
 
     public List<Message> getMessages() {
         return messages;
@@ -39,9 +14,28 @@ public abstract class User {
     
     public Message readMessage(int index) {
         return messages.get(index);
-    }
-    
+    }  
     abstract void writeMessage();
-
     abstract boolean sendMessage(int index);
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
