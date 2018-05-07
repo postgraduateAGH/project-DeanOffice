3286d334cd67aca08133d054dec4155e9054c23f
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-05-07 17:31:04 +0200

    update teacher

--- src/main/java/pl/edu/agh/ki/mwo/SchoolWebApp/controller/TeacherController.java
+++ src/main/java/pl/edu/agh/ki/mwo/SchoolWebApp/controller/TeacherController.java
@@ -18,92 +18,91 @@ import pl.edu.agh.ki.mwo.SchoolWebApp.repository.TeacherRepository;
 @Controller
 public class TeacherController {
 
-    @Autowired
-    private TeacherRepository teacherRepository;
+	@Autowired
+	private TeacherRepository teacherRepository;
 
-    @Autowired
-    private SchoolClassRepository schoolClassRepository;
+	@Autowired
+	private SchoolClassRepository schoolClassRepository;
 
-    @RequestMapping(value = "/Teachers")
-    public String listTeachers(Model model, HttpSession session) {
+	@RequestMapping(value = "/Teachers")
+	public String listTeachers(Model model, HttpSession session) {
 
-        model.addAttribute("teachers", teacherRepository.findAll());
-        return "teachersList";
-    }
+		model.addAttribute("teachers", teacherRepository.findAll());
+		return "teachersList";
+	}
 
-    @RequestMapping(value = "/DeleteTeacher", method = RequestMethod.POST)
-    public String deleteTeacher(@RequestParam(value = "teacherId", required = false) String teacherId, Model model, HttpSession session) {
+	@RequestMapping(value = "/DeleteTeacher", method = RequestMethod.POST)
+	public String deleteTeacher(@RequestParam(value = "teacherId", required = false) String teacherId, Model model,
+			HttpSession session) {
 
-        teacherRepository.deleteById(Long.valueOf(teacherId));
+		teacherRepository.deleteById(Long.valueOf(teacherId));
 
-        model.addAttribute("teachers", teacherRepository.findAll());
-        model.addAttribute("message", "Nauczyciel zostal dodany");
+		model.addAttribute("teachers", teacherRepository.findAll());
+		model.addAttribute("message", "Nauczyciel zostal dodany");
 
-        return "teachersList";
-    }
+		return "teachersList";
+	}
 
-/*    @RequestMapping(value = "/AddStudent")
-    public String displayAddStudentForm(Model model, HttpSession session) {
+	/*
+	 * @RequestMapping(value = "/AddStudent") public String
+	 * displayAddStudentForm(Model model, HttpSession session) {
+	 * 
+	 * model.addAttribute("classes", schoolClassRepository.findAll()); return
+	 * "studentForm"; }
+	 * 
+	 * @RequestMapping(value = "/CreateStudent", method = RequestMethod.POST) public
+	 * String createStudent(@RequestParam(value = "studentName", required = false)
+	 * String studentName,
+	 * 
+	 * @RequestParam(value = "studentSurname", required = false) String
+	 * studentSurname,
+	 * 
+	 * @RequestParam(value = "studentPesel", required = false) String studentPesel,
+	 * 
+	 * @RequestParam(value = "studentClass", required = false) String studentClass,
+	 * Model model, HttpSession session) {
+	 * 
+	 * Student student = new Student(); student.setName(studentName);
+	 * student.setSurname(studentSurname); student.setPesel(studentPesel);
+	 * SchoolClass schoolClass =
+	 * schoolClassRepository.findById(Long.valueOf(studentClass)).get();
+	 * student.setSchoolClass(schoolClass);
+	 * 
+	 * teacherRepository.save(student);
+	 * 
+	 * model.addAttribute("students", teacherRepository.findAll());
+	 * model.addAttribute("message", "Student zostal dodany");
+	 * 
+	 * return "studentsList"; }
+	 */
+	@RequestMapping(value = "/ShowUpdateTeacherForm")
+	public String showUpdateStudentForm(@RequestParam(value = "teacherId") Long teacherId, Model model,
+			HttpSession session) {
 
-        model.addAttribute("classes", schoolClassRepository.findAll());
-        return "studentForm";
-    }
+		Teacher student = teacherRepository.findById(teacherId).get();
+		model.addAttribute("teacher", student);
+		return "teacherUpdateForm";
+	}
 
-    @RequestMapping(value = "/CreateStudent", method = RequestMethod.POST)
-    public String createStudent(@RequestParam(value = "studentName", required = false) String studentName,
-            @RequestParam(value = "studentSurname", required = false) String studentSurname,
-            @RequestParam(value = "studentPesel", required = false) String studentPesel,
-            @RequestParam(value = "studentClass", required = false) String studentClass, Model model, HttpSession session) {
+	@RequestMapping(value = "/UpdateTeacher", method = RequestMethod.POST)
+	public String updateTeacher(@RequestParam(value = "teacherName", required = false) String teacherName,
+			@RequestParam(value = "teacherSurname", required = false) String teacherSurname,
+			@RequestParam(value = "teacherTitle", required = false) String teacherTitle,
+			@RequestParam(value = "teacherPesel") String teacherPesel,
+			@RequestParam(value = "teacherId") Long teacherId, Model model, HttpSession session) {
 
-        Student student = new Student();
-        student.setName(studentName);
-        student.setSurname(studentSurname);
-        student.setPesel(studentPesel);
-        SchoolClass schoolClass = schoolClassRepository.findById(Long.valueOf(studentClass)).get();
-        student.setSchoolClass(schoolClass);
+		Teacher teacher = teacherRepository.findById(teacherId).get();
+		teacher.setName(teacherName);
+		teacher.setSurname(teacherSurname);
+		teacher.setTitle(teacherTitle);
+		teacher.setPesel(teacherPesel);
 
-        teacherRepository.save(student);
+		// update
+		teacherRepository.save(teacher);
 
-        model.addAttribute("students", teacherRepository.findAll());
-        model.addAttribute("message", "Student zostal dodany");
+		model.addAttribute("teachers", teacherRepository.findAll());
+		model.addAttribute("message", "Dane nauczyciela zostaly zmienione");
 
-        return "studentsList";
-    }
-*/
-    @RequestMapping(value = "/ShowUpdateTeacherForm")
-    public String showUpdateStudentForm(@RequestParam(value = "teacherId") Long teacherId, Model model, HttpSession session) {
-
-        Teacher student = teacherRepository.findById(teacherId).get();
-        model.addAttribute("teacher", student);
-/*        if (schoolClassRepository.findAll() != null){
-            model.addAttribute("schoolClasses", schoolClassRepository.findAll());
-        }
-*/
-        return "teacherUpdateForm";
-    }
-/*
-    @RequestMapping(value = "/UpdateStudent", method = RequestMethod.POST)
-    public String updateSchoolClass(@RequestParam(value = "studentName", required = false) String studentName,
-            @RequestParam(value = "studentSurname", required = false) String studentSurname, @RequestParam(value = "studentPesel") String studentPesel,
-            @RequestParam(value = "studentId") Long studentId, @RequestParam(value = "studentClass") Long studentClass, Model model, HttpSession session) {
-
-
-
-        Student student = teacherRepository.findById(studentId).get();
-        student.setName(studentName);
-        student.setSurname(studentName);
-        student.setPesel(studentPesel);
-
-        SchoolClass schoolClass = schoolClassRepository.findById(studentClass).get();
-
-        student.setSchoolClass(schoolClass);
-
-        // update
-        teacherRepository.save(student);
-
-        model.addAttribute("students", teacherRepository.findAll());
-        model.addAttribute("message", "Student zostal dodany");
-
-        return "studentsList";
-    }
-*/}
+		return "teachersList";
+	}
+}
