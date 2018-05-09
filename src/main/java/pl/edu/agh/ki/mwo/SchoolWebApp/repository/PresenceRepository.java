d29e758a92e9a504b62cdcfe892c5ccf9e4f5b04
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-05-09 15:05:40 +0200

    presencesList and presences table changed
    view added + there was a problem with column which was declared as
    "INTEGER presence" instead of "presence INTEGER"

---
+++ src/main/java/pl/edu/agh/ki/mwo/SchoolWebApp/repository/PresenceRepository.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.ki.mwo.SchoolWebApp.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import pl.edu.agh.ki.mwo.SchoolWebApp.entity.Presence;
+
+public interface PresenceRepository extends CrudRepository<Presence, Long> {
+
+}
